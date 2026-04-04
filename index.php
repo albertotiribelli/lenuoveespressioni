9301c761452589e11807a8e9949880cb73263e49
albertotiribelli <alberto.tiribelli@thoughtworks.com>
2026-04-04 14:48:17 +0200

    feat(lenuoveespressioni): primo commit con sito vecchio

---
+++ index.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html><head>
+ <meta charset="utf-8" />
+ <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;" />
+ <meta description="Index of index.php" />
+
+ <title>index.php</title>
+ <style type="text/css">
+
+  *    { margin: 0; padding: 0; }
+  html { -webkit-text-size-adjust: none; } 
+  body { font: 16px sans-serif; color: #333; background: white; line-height: 3em; }
+
+  a { text-decoration: none; color: #06c; }
+  a:visited { color: #04a; }
+  a:hover, a:focus { text-decoration: underline; }
+
+  h1   { text-align: right; font-size: 1em; line-height: 2em; margin: 0.5em;} 
+  h1 a { padding: 1em 0; }
+
+  ul   { list-style-type: none; border: solid #ccc; border-width: 1px 0; }
+  li a { display: block; }
+  img  { vertical-align: middle; margin-top: -3px; padding: 0 0.2em; }
+  li:nth-child(odd) { background: #eee; }
+
+  #summary, #about { text-align: right; padding-right: 0.5em; }
+  #about { font-size: 80%; color: #444; padding-right: 0.625em; }
+
+  @media only screen and (min-width: 640px) {
+    body { width: 80%; margin: auto; line-height: 2em; }
+    img { padding: 0 0.5em; }
+  }
+
+ </style>
+
+</head><body>
+
+ <h1>index.php</h1>
+
+ <ul>
+   <li><a href="?path=css"><img src="./elenconaut/icons/directory.png" title="directory">css</a></li>
+   <li><a href="?path=img"><img src="./elenconaut/icons/directory.png" title="directory">img</a></li>
+   <li><a href="?path=js"><img src="./elenconaut/icons/directory.png" title="directory">js</a></li>
+   <li><a href="?path=src"><img src="./elenconaut/icons/directory.png" title="directory">src</a></li>
+   <li><a href="./compagnia.html"><img src="./elenconaut/icons/text-html.png" title="text-html">compagnia.html</a></li>
+   <li><a href="./index.html"><img src="./elenconaut/icons/text-html.png" title="text-html">index.html</a></li>
+   <li><a href="./spettacoli.html"><img src="./elenconaut/icons/text-html.png" title="text-html">spettacoli.html</a></li>
+   <li><a href="./storia.html"><img src="./elenconaut/icons/text-html.png" title="text-html">storia.html</a></li>
+  </ul>
+
+  <p id="summary">
+   17.95KB ( 4 files,
+   4 directories )</p>
+
+ <p id="about">Generated with <a href="http://dreadnaut.altervista.org/progetti/elenconaut/">Elenconaut 0.3</a></p>
+
+ <script type="text/javascript">
+   /mobile/i.test(navigator.userAgent) && window.addEventListener("load", function() { setTimeout(function() { window.scrollTo(0, 1); }, 300); });
+ </script>
+
+</body></html>
+
