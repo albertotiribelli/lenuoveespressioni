2ddd6350e582dcc615923e599a46d50f0d73f796
albertotiribelli <alberto.tiribelli@thoughtworks.com>
2026-04-04 15:23:11 +0200

    feat(lenuoveespressioni): deleting old site

--- js/permanent.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-   $(".jumbotron").css("height", $(window).innerHeight / 4.2);               
-});
-
-$(window).resize(function(){
-   $(".jumbotron").css("height", $(window).innerHeight / 4.2);               
-});
